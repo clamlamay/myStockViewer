dca00cf37e25be6ccaf4b3ea56b670a0db17694e
Christopher <christopher.lamlamay@gmail.com>
2018-09-23 10:46:01 -0500

    added innerHTML

--- scripts/app.js
+++ scripts/app.js
@@ -3,16 +3,38 @@ console.log('window is loaded')
 	$('button').on('click', function(){
 	console.log('button is working');
 	var stock = $('input').val();
-
 		$.ajax({
 			url: 'https://api.iextrading.com/1.0/stock/'+stock+'/batch?types=quote',
 			type: 'Get',
 			dataType: 'json',
 			success: function(data) {
-				console.log(data.name);	
+				console.log(data);
+				console.log(data.quote.symbol);
+				console.log(data.quote.close);
+				var symbol = data.quote.symbol;
+				var close = data.quote.close
+				document.getElementById("message").innerHTML = symbol + " closed at " + close;
 			},
 			error: function(err) {
 				console.log(err);
 			}
 		})
-})
+		// $.ajax({
+		// 	url: 'https://api.iextrading.com/1.0/stock/'+symbol+'/price'
+		// 	type: 'Get',
+		// 	dataType: 'json',
+		// 	success: function(data){
+		// 		console.log(data)
+		// 	},
+		// 	error: function () {
+		// 		console.log(err);
+		// 	}
+		// })
+
+	})
+
+
+
+
+
+	
